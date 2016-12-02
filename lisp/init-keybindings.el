7076681e680aafcbad03c400fbb60d25a996c61b
Edge <baiban1994@gmail.com>
2016-12-02 12:58:02 +0800

    improve indent region

--- lisp/init-keybindings.el
+++ lisp/init-keybindings.el
@@ -13,4 +13,6 @@
 (global-set-key (kbd "C-h C-v") 'find-variable)
 (global-set-key (kbd "C-h C-k") 'find-function-on-key)
 
+(global-set-key (kbd "C-M-\\") 'indent-region-or-buffer)
+
 (provide 'init-keybindings)
